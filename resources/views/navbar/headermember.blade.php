55572e0654b51ee6b9170095cbd9a646173bddb3
STEVEN ALEXANDER GRANT <steven.grant@binus.ac.id>
2022-12-13 20:25:36 +0700

    final fix

--- resources/views/navbar/headermember.blade.php
+++ resources/views/navbar/headermember.blade.php
@@ -1,30 +1,44 @@
 <nav class="navbar navbar-expand-lg bg-black navbar-dark">
-    <div class="container-fluid m-1">
-      <a class="navbar-brand" href="#"><h3>MAI BOTIQUE</h3></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+   <div class="container-fluid m-1">
+      <a class="navbar-brand" href="{{ route('index') }}">
+         <h3>MAI BOTIQUE</h3>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link {{ Route::is('index') ? 'active' : '' }}" aria-current="page" href="{{ route('index') }}"><h5>Home</h5></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ Route::is('index.search') ? 'active' : '' }} " href="{{ route('index.search') }}"><h5>Search</h5></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ Route::is('cart.index') ? 'active' : '' }}" href="{{ route('cart.index') }}"><h5>Cart</h5></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ Route::is('trx') ? 'active' : '' }}" href="{{ route('trx') }}"><h5>History</h5></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ Route::is('profile') ? 'active' : '' }}" href="{{ route('profile') }}"><h5>Profile</h5></a>
-          </li>
-        </ul>
-        <div class="d-flex">
+         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+               <a class="nav-link {{ Route::is('index') ? 'active' : '' }}" aria-current="page"
+                  href="{{ route('index') }}">
+                  <h5>Home</h5>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link {{ Route::is('index.search') ? 'active' : '' }} " href="{{ route('index.search') }}">
+                  <h5>Search</h5>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link {{ Route::is('cart.index') ? 'active' : '' }}" href="{{ route('cart.index') }}">
+                  <h5>Cart</h5>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link {{ Route::is('trx') ? 'active' : '' }}" href="{{ route('trx') }}">
+                  <h5>History</h5>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link {{ Route::is('profile') ? 'active' : '' }}" href="{{ route('profile') }}">
+                  <h5>Profile</h5>
+               </a>
+            </li>
+         </ul>
+         <div class="d-flex">
             <a href="/logout"><button class="btn btn-primary">Sign Out</button></a>
-        </div>
+         </div>
       </div>
-    </div>
-  </nav>
+   </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,18 @@
 @section('title', 'Maiboutique')
 
 @section('content')
-
-<img src="{{asset('img/bg-home.png')}}" alt="" style="height: 100vh; width: 100%; position: absolute; object-fit: cover; z-index: -1;">
-
-
-<div class="d-flex justify-content-center align-items-center" style="height: 92.5vh">
-    <div class="card bg-dark text-center text-white" style="width: 30rem;">
-        <div class="card-body">
+   <img src="{{ asset('img/bg-home.png') }}" alt=""
+      style="height: 100vh; width: 100vw; position: absolute; top: 0; left:0; z-index: -1;">
+   <div class="d-flex justify-content-center align-items-center position-absolute"
+      style="left: 50%; top:50%; transform: translate(-50%, -50%)">
+      <div class="card bg-dark text-center text-white" style="width: 30rem;">
+         <div class="card-body">
             <h3>Welcome To MaiBoutique</h3>
             <h5>Online Boutique that Provides Everything for You</h5>
-            <a href="{{ route('auth.register')}}"><button class="btn btn-primary mt-1" type="button">Sign Up Now</button></a>
-
-        </div>
-    </div>
-</div>
+            <a href="{{ route('auth.register') }}"><button class="btn btn-primary mt-1" type="button">Sign Up
+                  Now</button></a>
+         </div>
+      </div>
+   </div>
 
 @endsection
